07aedddd6f75c6db4ddc9f0d5d5c0b32fa24e3a6
Shun Usami <usatie@gmail.com>
2023-06-11 16:56:23 +0900

    Add headers

---
+++ playground/youtube_webserv/README.md
@@ -0,0 +1,4 @@
+# webserv
+This code is from this repo.
+I try to follow the code below to understand how to write a web server.
+https://github.com/infraredCoding/cerveur
---
+++ playground/youtube_webserv/include/Routes.h
@@ -0,0 +1,21 @@
+#ifndef ROUTES_H
+# define ROUTES_H
+
+# include <stdlib.h>
+# include <string.h>
+
+struct Route {
+	char* key;
+	char* value;
+
+	struct Route *left, *right;
+};
+
+struct Route* initRoute(char* key, char* value);
+
+struct Route* addRoute(struct Route * root, char* key, char* value);
+struct Route* search(struct Route * root, char* key);
+
+void inorder(struct Route *root );
+
+#endif
