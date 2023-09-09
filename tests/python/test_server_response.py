7fc9309a3374a7185803de778c0cad635e2a8a30
Junya Kosaka <kosakajunya@gmail.com>
2023-09-09 15:35:21 +0900

    response.apparent_encoding

--- tests/python/test_server_response.py
+++ tests/python/test_server_response.py
@@ -3,16 +3,19 @@ import sys
 
 def test_get_request():
     server_url = 'http://localhost:8181'
-    path = '/tests/static/a.text'
+    file_path = '/tests/static/a.text'
 
-    response = requests.get(server_url + path)
+    response = requests.get(server_url + file_path)
 
-    text_content = open('.' + path, 'rb').read()
+    # using 'rb' mode to handle binary data including cases with \r\n and \n characters.
+    text_content = open('.' + file_path, 'rb').read()
 
     assert response.status_code == 200
     assert response.headers['Content-Type'] == 'text/plain'
     assert response.headers['Content-Length'] == str(len(text_content))
 
+    # ensure that the response encoding matches the apparent encoding, especially for non-ASCII characters.
+    response.encoding = response.apparent_encoding
     assert response.text == text_content.decode()
 
 if __name__ == '__main__':
@@ -20,6 +23,4 @@ if __name__ == '__main__':
         test_get_request()
         sys.exit(0)
     except AssertionError as e:
-        # print(e)
         raise e
-        sys.exit(1)
