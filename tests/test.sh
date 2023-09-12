f7f62429f7d32956ceed2be996d116a5f59a9c86
Shun Usami <usatie@gmail.com>
2023-09-12 15:32:43 +0900

    :sparkles: Use Index directive
    - Add tests for root directive
    - Add tests for alias directive (a test in the subject pdf included)
    - Add tests for index directive (single file, multiple files)

--- tests/test.sh
+++ tests/test.sh
@@ -8,11 +8,12 @@ WEBSERV_PORT=8181
 # wait for server warm up
 sleep 1.0
 rm -f error.log
+mkdir -p /tmp/www/pouic/toto && echo "pouet" > /tmp/www/pouic/toto/pouet
 
 # 2. Tests
 # arg for save error count
 cnt=0
-for i in {1..14}; do
+for i in {1..21}; do
   echo -n "Test${i}   : " | tee -a error.log
   echo "" >>error.log
   if [[ "$(uname -s)" == "Linux" ]]; then
