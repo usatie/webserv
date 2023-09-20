dd684b0cddabaf98c3f83d13cc3bf39b86b0ad6a
Shun Usami <usatie@gmail.com>
2023-09-20 14:39:20 +0900

    :sparkles: Add Listen.hpp

--- srcs/config/Config.hpp
+++ srcs/config/Config.hpp
@@ -7,6 +7,7 @@
 #include <vector>
 
 #include "ConfigParser.hpp"
+#include "Listen.hpp"
 
 namespace config {
   class Config;
@@ -37,19 +38,6 @@ namespace config {
   std::ostream &operator<<(std::ostream &os, const Config &cf);
 }
 
-class config::Listen {
- public:
-  std::string address;  // ip address, wildcard, or hostname
-  int port;
-  bool configured;
-  struct sockaddr_storage addr;
-  socklen_t addrlen;
-  Listen(const std::string &address, const int &port)
-      : address(address), port(port), configured(true), addr(), addrlen(0) {
-    if (address.empty()) this->address = "*";
-  }
-  Listen() : address("*"), port(8181), configured(false) {}
-};
 class config::ErrorPage {
  public:
   std::vector<int> codes;
